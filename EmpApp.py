a1be22362dbee04fc390636afdbdb4b5aad46d05
JiekeeKhoo <100903368+JiekeeKhoo@users.noreply.github.com>
2023-05-07 16:22:48 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -300,8 +300,7 @@ def payroll():
 
     # loop through all attendance data for the employee and sum up the duration
     for attendance_data in emp_data_attendance:
-        duration_str = attendance_data[5]
-        duration = datetime.datetime.strptime(duration_str, "%H:%M:%S").time()
+        duration = attendance_data[5]
         total_duration += datetime.timedelta(hours=duration.hour, minutes=duration.minute, seconds=duration.second)
 
     # calculate total duration in hours
