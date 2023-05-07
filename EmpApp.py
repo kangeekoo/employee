66a4481e106746438df8032fa9a75ec642288de7
kangeekoo <100903919+kangeekoo@users.noreply.github.com>
2023-05-07 12:17:19 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -3,7 +3,7 @@ from pymysql import connections
 import os
 import boto3
 from config import *
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from datetime import datetime, timedelta
 
